e750b0f9a43f05c2c8ebf29f2598c68e2c460153
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-13 12:30:55 +0000

    Ex 8.5: Add new data/ keys

--- directory.rb
+++ directory.rb
@@ -1,17 +1,34 @@
 def input_students
-  puts "Please eneter the names of the students"
-  puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
+  puts "Please enter the information of the student:"
+  puts "Name: "
   name = gets.chomp
+  puts "Cohort: "
+  cohort = gets.chomp
+  puts "Country of birth"
+  country = gets.chomp
+  puts "Hobbies"
+  hobbies = gets.chomp
+  
+
+ 
+  students = []
+  
   # while the name is not empty, repeat his code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, country: country, hobbies: hobbies}
     puts "Now we have #{students.count} students"
-    # get another name from the user
+    puts "Please add new student information or return to finish"
+    puts "Name: "
     name = gets.chomp
+    if !name.empty?
+      puts "Cohort: "
+      cohort = gets.chomp
+      puts "Country of birth"
+      country = gets.chomp
+      puts "Hobbies"
+      hobbies = gets.chomp
+    end
   end
   # return the array of students
   students
@@ -25,7 +42,10 @@ end
 def print (students)
   idx = 0
   while idx < students.length
-    puts "#{idx+1}. #{students[idx][:name]} (#{students[idx][:cohort]} cohort)"
+    puts "#{idx+1}. Name: #{students[idx][:name]}" 
+    puts "\s\s Cohort: #{students[idx][:cohort]}"
+    puts "\s\s Country:#{students[idx][:country]}"  
+    puts "\s\s Hobbies: #{students[idx][:hobbies]}"
     idx += 1
   end
 end
