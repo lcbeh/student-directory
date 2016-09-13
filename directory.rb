697debf3c5e5edf37331d4e9e92bf41bb3d37cf8
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-13 14:02:54 +0000

    Ex 8.7: Add default value

--- directory.rb
+++ directory.rb
@@ -1,42 +1,38 @@
 def input_students
+  
   puts "Please enter the information of the student:"
   puts "Name: "
   name = gets.chomp
-  puts "Cohort: "
-  cohort = gets.chomp
-  puts "Country of birth"
-  country = gets.chomp
-  puts "Hobbies"
-  hobbies = gets.chomp
   
-
- 
   students = []
   
-  # while the name is not empty, repeat his code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, hobbies: hobbies}
-    puts "Now we have #{students.count} students"
-    puts "Please add new student information or return to finish"
-    puts "Name: "
-    name = gets.chomp
-    if !name.empty?
+  # while the name is not empty, repeat this code
+  while !name.empty? 
       puts "Cohort: "
-      cohort = gets.chomp
-      puts "Country of birth"
+      cohort = gets.chomp.capitalize.to_sym
+      cohort.empty? ? cohort = :November : cohort
+      puts "Country of birth: "
       country = gets.chomp
-      puts "Hobbies"
-      hobbies = gets.chomp
-    end
+      country.empty? ? country = "UK" : country
+      puts "Best skill: "
+      skill = gets.chomp
+      skill.empty? ? skill = "Coding" : skill
+    # add the student hash to the array
+      students << {name: name, cohort: cohort, country: country, skill: skill}
+      puts "Now we have #{students.count} students"
+      puts "Please add new student information or return to finish"
+      puts "Name: "
+      name = gets.chomp 
   end
   # return the array of students
   students
 end
 
-def print_header  
-  puts "The students of Unreal Academy"
-  puts "-------------"
+def print_header 
+  title = "The students of Unreal Academy"
+  line = "-------------------------------------------------"
+  puts title.center(50)
+  puts line.center(50)
 end
 
 def print (students)
@@ -44,8 +40,8 @@ def print (students)
   while idx < students.length
     puts "#{idx+1}. Name: #{students[idx][:name]}" 
     puts "\s\s Cohort: #{students[idx][:cohort]}"
-    puts "\s\s Country:#{students[idx][:country]}"  
-    puts "\s\s Hobbies: #{students[idx][:hobbies]}"
+    puts "\s\s Country: #{students[idx][:country]}"  
+    puts "\s\s Best skill: #{students[idx][:skill]}"
     idx += 1
   end
 end
