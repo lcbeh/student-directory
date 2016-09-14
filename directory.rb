b2147c350c65f8c7d0c092fcb078e1404df53323
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-14 08:59:26 +0000

    Refactor methods and add instance variable

--- directory.rb
+++ directory.rb
@@ -1,39 +1,41 @@
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu ad ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets. chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def input_students
-
   puts "Please enter the information of the student:"
   puts "Name: "
-  name = gets
-  # another way to remove new line without using chomp
-  name.gsub!("\n", "")
-
-  students = []
+  name = gets.chomp
 
   # while the name is not empty, repeat this code
   while !name.empty?
@@ -47,18 +49,16 @@ def input_students
       skill = gets.chomp
       skill.empty? ? skill = "Coding" : skill
     # add the student hash to the array
-      students << {name: name, cohort: cohort, country: country, skill: skill}
-      if students.count == 1
-        puts "Now we have #{students.count} student"
+      @students << {name: name, cohort: cohort, country: country, skill: skill}
+      if @students.count == 1
+        puts "Now we have #{@students.count} student"
       else
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
       end
       puts "Please add new student information or return to finish"
       puts "Name: "
       name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -68,22 +68,22 @@ def print_header
   puts line.center(50)
 end
 
-def print (students)
-    p students
+def print_students_list
     idx = 0
-    while idx < students.length
-      puts "#{idx+1}. Name: #{students[idx][:name]}"
-      puts "\s\s Cohort: #{students[idx][:cohort]}"
-      puts "\s\s Country: #{students[idx][:country]}"
-      puts "\s\s Best skill: #{students[idx][:skill]}"
+    while idx < @students.length
+      puts "#{idx+1}. Name: #{@students[idx][:name]}"
+      puts "\s\s Cohort: #{@students[idx][:cohort]}"
+      puts "\s\s Country: #{@students[idx][:country]}"
+      puts "\s\s Best skill: #{@students[idx][:skill]}"
+      puts
       idx += 1
     end
 end
 
-def print_by_cohort (students)
+def print_by_cohort
   # optional step: creating a list of existing cohorts
   existing_cohorts = []
-  students.map do |student|
+  @students.map do |student|
     existing_cohorts << student[:cohort]
   end
   existing_cohorts.uniq!
@@ -94,35 +94,35 @@ def print_by_cohort (students)
   cohort_choice = gets.chomp.to_sym
 
   # selecting students from a chosen cohort
-    students.map do |student|
-      if student[:cohort] == cohort_choice
-        puts "#{student[:name]}"
-      end
+  @students.map do |student|
+    if @student[:cohort] == cohort_choice
+        puts "#{@student[:name]}"
     end
+  end
 end
 
-def print (students)
-  if students.count < 1
+def print
+  if @students.count < 1
     puts "There is no student currently."
   else
     idx = 0
-    while idx < students.count
-      puts "#{idx+1}. Name: #{students[idx][:name]}"
-      puts "\s\s Cohort: #{students[idx][:cohort]}"
-      puts "\s\s Country: #{students[idx][:country]}"
-      puts "\s\s Best skill: #{students[idx][:skill]}"
+    while idx < @students.count
+      puts "#{idx+1}. Name: #{@students[idx][:name]}"
+      puts "\s\s Cohort: #{@students[idx][:cohort]}"
+      puts "\s\s Country: #{@students[idx][:country]}"
+      puts "\s\s Best skill: #{@students[idx][:skill]}"
       idx += 1
     end
   end
 end
 
-def print_footer (students)
-  if students.count == 0
-    return
-  elsif students.count == 1
+def print_footer
+  if @students.count == 0
+    puts " We don't have any student currently."
+  elsif @students.count == 1
     puts "Currently, we have 1 great student."
   else
-    puts "Overall, we have #{students.count} great students."
+    puts "Overall, we have #{@students.count} great students."
   end
 end
 
