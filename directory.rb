1132a4eec8bf349988301c64247322f47d850745
Li Chien Beh <lichien2010@yahoo.co.uk>
2016-09-15 10:11:26 +0000

    Ex 14.3: Enable saving to the right database file

--- directory.rb
+++ directory.rb
@@ -11,7 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save students to database"
-  puts "4. Load students database"
+  # puts "4. Load students database"
   puts "9. Exit"
 end
 
@@ -23,19 +23,14 @@ def process(selection)
     show_students
   when "3"
     save_students
-  when "4"
-    load_students
+  # function removed. Database is loaded automatically at start
+  # when "4"
+  # load_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
   end
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
 end
 
 def input_students
@@ -54,9 +49,8 @@ def input_students
       puts "Best skill: "
       skill = STDIN.gets.chomp
       skill.empty? ? skill = "Coding" : skill
+      # store students only if information is new
       store_students(name, cohort, country, skill)
-      # discard any repetitive student data
-      @students.uniq!  
       if @students.count == 1
         puts "Now we have #{@students.count} student"
       else
@@ -72,46 +66,10 @@ def store_students(name, cohort, country, skill)
   @students << {name: name, cohort: cohort, country: country, skill: skill}
 end
 
-def save_students
-  # open file
-  # convert hashes into array and into csv format
-  # write file
-  # close file
-  file = File.open("students.csv","w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort], student[:country], student[:skill]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-    file.close
-    puts "Data saved."
-end
-
-
-# create a method which can load file is argument is given in command line
-def try_load_students
-  # check if argument is given
-  # if so, does the file exist? pass it as argument
-  # if such file doesn't exist, let the user know.
-  filename = ARGV.first
-  if filename.nil?
-    load_students 
-  elsif File.exists?(filename)
-    load_students(filename)
-  else
-    puts "Sorry, #{filename} doesn't exist."
-  end
-end
-
-# create a method which can load any file
-def load_students (filename = "students.csv")   # students.csv as default argument
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort, country, skill = line.chomp.split(",")
-    store_students(name, cohort, country, skill)
-  end
-  file.close
-  puts "Database loaded."
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -122,39 +80,6 @@ def print_header
 end
 
 def print_students_list
-    idx = 0
-    while idx < @students.length
-      puts "#{idx+1}. Name: #{@students[idx][:name]}"
-      puts "\s\s Cohort: #{@students[idx][:cohort]}"
-      puts "\s\s Country: #{@students[idx][:country]}"
-      puts "\s\s Best skill: #{@students[idx][:skill]}"
-      puts
-      idx += 1
-    end
-end
-
-def print_by_cohort
-  # optional step: creating a list of existing cohorts
-  existing_cohorts = []
-  @students.map do |student|
-    existing_cohorts << student[:cohort]
-  end
-  existing_cohorts.uniq!
-
-  puts "The current cohorts are:"
-  existing_cohorts.each {|i| puts i.id2name}
-  puts "To view a list of students in a particular cohort, please specify the cohort: "
-  cohort_choice = STDIN.gets.chomp.to_sym
-
-  # selecting students from a chosen cohort
-  @students.map do |student|
-    if @student[:cohort] == cohort_choice
-        puts "#{@student[:name]}"
-    end
-  end
-end
-
-def print
   if @students.count < 1
     puts "There is no student currently."
   else
@@ -176,6 +101,50 @@ def print_footer
     puts "Currently, we have 1 great student."
   else
     puts "Overall, we have #{@students.count} great students."
+  end
+end
+
+def save_students
+  # find the correct database file to save data 
+  # open file
+  # convert hashes into array and into csv format
+  # write file
+  # close file
+  filename = ARGV.first
+  filename.nil? ? filename = "students.csv" : filename
+  file = File.open(filename,"w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:country], student[:skill]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+    file.close
+    puts "Data saved."
+end
+
+# create a method which can load any file
+def load_students (filename = "students.csv")   # students.csv as default argument
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort, country, skill = line.chomp.split(",")
+    store_students(name, cohort, country, skill)
+  end
+  file.close
+  puts "Database loaded."
+end
+
+# create a method which can load file if argument is given in command line
+def try_load_students
+  # check if argument is given
+  # if so, does the file exist? pass it as argument
+  # if such file doesn't exist, let the user know.
+  filename = ARGV.first
+  if filename.nil?
+    load_students
+  elsif File.exists?(filename)
+    load_students(filename)
+  else
+    puts "Sorry, #{filename} doesn't exist."
   end
 end
 
